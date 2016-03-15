2d445ad656c2765aae192d36b211128797a6462c
kapoorlab <kapoorconsciousness@gmail.com>
2016-03-15 16:35:45 +0100

    Clean push

--- src/main/java/varun/PlotFunctionPush.java
+++ src/main/java/varun/PlotFunctionPush.java
@@ -14,148 +14,138 @@ import net.imglib2.type.numeric.real.FloatType;
 import util.ImgLib2Util;
 
 public class PlotFunctionPush {
-	// how you started now, draw one function after the other into the image
+	
 
-	// for (x-img.dimensions(0)/2)^2+(y-image.dimensions(1)/2)^2 = radius^2,
-	// radius = image.dimensions(0)/2
+	public static <T extends RealType<T>> void push(Img<T> inputimage, RandomAccessible<T> imgout, double[] min,
+			double[] max) {
 
-	public static <T extends RealType<T>> void push (Img<T> inputimage,RandomAccessible<T> imgout, double[] min, double[] max){
-		
 		int n = inputimage.numDimensions();
 		final int[] position = new int[n];
-		final int[] newpositionfunctone = new int[n];
-		final int[] newpositionfuncttwo = new int[n];
-		final int[] newpositionfunctthree = new int[n];
-		final double[] realposfunctone = new double[n];
-		final double[] realposfuncttwo = new double [n];
-		final double[] realposfunctthree = new double [n];
-		
-	
-		
+
+		final int[] realposone = new int[n];
+		final int[] realpostwo = new int[n];
+		final int[] realposthree = new int[n];
+
+		final double[] realpos = new double[n];
+		double functionone, functiontwo, functionthree;
+		int newposone, newpostwo, newposthree;
+		final double sigma = 100.0;
+
 		double[] delta = new double[n];
-		
-		for (int d = 0; d<n;++d){
-		delta[d] = (max[d] - min[d])/inputimage.dimension(d);
-		
+
+		for (int d = 0; d < n; ++d) {
+			delta[d] = (max[d] - min[d]) / inputimage.dimension(d);
+
 		}
+
 		
-		double center =inputimage.dimension(0)/2*delta[0]+min[0]; // For centering function at the middle of the grid.
+		// For centering Gaussian and Quadratic function at the center of the image
+		
+		double center = (inputimage.dimension(0) / 2) * delta[0] + min[0]; 
 		
 		final Cursor<T> inputcursor = inputimage.localizingCursor();
 		final RandomAccess<T> outbound = imgout.randomAccess();
-		
-		while(inputcursor.hasNext()){
-		inputcursor.fwd();
-		inputcursor.localize(position);
-		
-		
-			realposfunctone[0] = position[0]*delta[0]+min[0]; // Transforming from pixels to function space
-			
-			realposfunctone[1] = -100*Math.exp(-Math.pow((realposfunctone[0]-center),2)/100); // Gaussian function
-			
-			realposfuncttwo[0] = realposfunctone[0];
-			realposfuncttwo[1] = 10*Math.sin(Math.toRadians(realposfunctone[0]*10)); // Sin function
-			
-			realposfunctthree[0] = realposfunctone[0];
-			realposfunctthree[1] = -100*(-Math.pow((realposfunctone[0]-center),2)/100); // Quadratic function
-			
-		newpositionfunctone[1] = (int) Math.round((realposfunctone[1]-min[1])/delta[1]); // Transforming back the y coordinate for Gaussian
-		newpositionfunctone[0] = position[0];
-		
-		
-		newpositionfuncttwo[1] = (int) Math.round((realposfuncttwo[1]-min[1])/delta[1]); // Transforming back the y coordinate for Sin
-		newpositionfuncttwo[0] = position[0];
-		
-		
-		newpositionfunctthree[1] = (int) Math.round((realposfunctthree[1]-min[1])/delta[1]);// Transforming back the y coordinate for Quadratic
-		newpositionfunctthree[0] = position[0];
-		
 
-		if (newpositionfunctone[1] < inputimage.dimension(1)){
-		
-	
-		
-		outbound.setPosition(newpositionfunctone);
-		
-		
-	outbound.get().setReal(1);
-		
-		
-		}
-	
-		if (newpositionfuncttwo[1] < inputimage.dimension(1)){
+		while (inputcursor.hasNext()) {
+			inputcursor.fwd();
+			inputcursor.localize(position);
+
+			// Forward transformation
 			
+			for (int d = 0; d < n; ++d) {
+
+				realpos[d] = inputcursor.getDoublePosition(d) * delta[d] + min[0];
+
+			}
+			// Gaussian function
+
+			functionone = -100 * Math.exp(-Math.pow((realpos[0] - center), 2) / sigma); 
+
+			// Sin function
 			
+			functiontwo = 10 * Math.sin(Math.toRadians(realpos[0] * 10)); 
 			
-		outbound.setPosition(newpositionfuncttwo);
+			// Quadratic function
+
+			functionthree = -100 * (-Math.pow((realpos[0] - center), 2) / 100); 
+																			 
+
+			// Back transformation
 			
+			//Transforming back the y coordinate for Gaussian
 			
-		outbound.get().setReal(1);
+			newposone = (int) Math.round((functionone - min[1]) / delta[1]); 
 			
+			//Transforming back the y coordinate for Sin function
 			
-			}	
-		if (newpositionfunctthree[1] < inputimage.dimension(1)){
+			newpostwo = (int) Math.round((functiontwo - min[1]) / delta[1]); 
 			
+			//Transforming back the y coordinate for Quadratic function
 			
-			
-			outbound.setPosition(newpositionfunctthree);
-				
-				
-		outbound.get().setReal(1);
-			
-				
-				}	
-		
-		
-		
+			newposthree = (int) Math.round((functionthree - min[1]) / delta[1]);
+
+			// To get the new pixels, first set them to original values and then
+			// change the y value to the new one
+
+			inputcursor.localize(realposone);
+			inputcursor.localize(realpostwo);
+			inputcursor.localize(realposthree);
+
+			realposone[1] = newposone;
+			realpostwo[1] = newpostwo;
+			realposthree[1] = newposthree;
+
+			// To make sure that the new y values are in the proper range, plot
+			// only when in range
+
+			// Plotting for function one
+			if (newposone < inputimage.dimension(1)) {
+
+				outbound.setPosition(realposone);
+
+				outbound.get().setReal(1);
+
+			}
+
+			// Plotting for function two
+			if (newpostwo < inputimage.dimension(1)) {
+
+				outbound.setPosition(realpostwo);
+
+				outbound.get().setReal(1);
+
+			}
+
+			// Plotting for function three
+			if (newposthree < inputimage.dimension(1)) {
+
+				outbound.setPosition(realposthree);
+
+				outbound.get().setReal(1);
+
+			}
+
 		}
 	}
-
-	
 
 	public static void main(String[] args) {
 
 		final Img<FloatType> inputimage = ImgLib2Util.openAs32Bit(new File("src/main/resources/1line.tif"));
 
-		
-		
 		ImageJFunctions.show(inputimage);
-		
-		double[] min ={-150,-150};
-		double[] max ={150,150};
-		
-		
-		
-		
-		
-		
-		
-		
-		int pixelsY = (int) Math.round((max[1]-min[1]));
-		int pixelsX = (int) Math.round((max[0]-min[0]));
-		
-		
+
+		double[] min = { -150, -150 };
+		double[] max = { 150, 150 };
+
+		int pixelsY = (int) Math.round((max[1] - min[1]));
+		int pixelsX = (int) Math.round((max[0] - min[0]));
 
 		FinalInterval intervalquad = new FinalInterval(new long[] { pixelsX, pixelsY });
-		
-		
 
-	
-		
 		final Img<FloatType> houghquadimage = new ArrayImgFactory<FloatType>().create(intervalquad, new FloatType());
-		
-		
-		
-		
-		
-		
 
-		
-		
-	push(inputimage, houghquadimage, min, max);
+		push(inputimage, houghquadimage, min, max);
 
-		
-		
 		ImageJFunctions.show(houghquadimage).setTitle("Test function");
 
 	}
