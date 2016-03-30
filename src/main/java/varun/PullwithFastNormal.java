733fb1106a5f6d00c0f77403135435e6d3015fc7
kapoorlab <kapoorconsciousness@gmail.com>
2016-03-30 11:46:02 +0200

    Another approximate circle

--- src/main/java/varun/PullwithFastNormal.java
+++ src/main/java/varun/PullwithFastNormal.java
@@ -31,10 +31,8 @@ public class PullwithFastNormal {
 
 		final double[] realpos = new double[n];
 
-		final double[] secondposition = new double[n];
-		final double[] secondrealpos = new double[n];
 		double[] actualposition = new double[n];
-		double[] tmpactualposition = new double[n];
+		
 		double sigmasq = 0, sigma;
 
 		double[] delta = new double[n];
@@ -43,41 +41,48 @@ public class PullwithFastNormal {
 
 			delta[d] = (max[d] - min[d]) / (imgout.dimension(d));
 
-			sigmasq += Math.pow( delta[d], 2);
+			sigmasq += Math.pow(delta[d], 2);
 
 		}
 
 		sigma = Math.sqrt(sigmasq);
-		double[] center = { -10,10 }; double radius = 40;
+		double[] center = { 0, 0 };
+		double radius = 40;
 		final Cursor<T> inputcursor = Views.iterable(imgout).localizingCursor();
 		final RandomAccess<T> outbound = imgout.randomAccess();
 		final RandomAccess<T> circle = imgout.randomAccess();
-		
-		
+		long[] gradient = new long[n];
+
 		while (inputcursor.hasNext()) {
 			inputcursor.fwd();
 			inputcursor.localize(position);
 			
-			// Forward transformation
 			for (int d = 0; d < n; ++d)
 				realpos[d] = position[d] * delta[d] + min[d];
-			
+
 			outbound.setPosition(inputcursor);
 			
 			double mindistance = Double.MAX_VALUE;
 			double secmindistance = Double.MAX_VALUE;
-			// To set the pixel intensity to the distance from the curve
+			
 			double distance = 0;
-			double intensity;
-			for (double theta =0; theta<=360;++theta){
-				circle.setPosition(Math.round(center[0]-radius*Math.sin(Math.toRadians(theta))), 0);
-				circle.setPosition(Math.round((center[1]+radius*Math.cos(Math.toRadians(theta)))), 1);
-				double newx=  circle.getDoublePosition(0);
-				double newy=  circle.getDoublePosition(1);
+			double intensity= 0;
+			double step = 1.0;
+
+			for (double theta = 0; theta <= 360; theta+=step) {
+			
+					gradient[0] = -Math.round(radius * Math.cos(Math.toRadians(theta)));
+					gradient[1] = -Math.round(radius * Math.sin(Math.toRadians(theta)));
+			
+
+				for (int d = 0; d < n; ++d)
+					circle.setPosition(Math.round(center[d] + gradient[d]), d);
+
+				double newx = circle.getDoublePosition(0);
+				double newy = circle.getDoublePosition(1);
 				
-				
-				final double distanceline = Finaldistance.disttocurve(new double[] {newx,newy}, realpos, newy,
-						-(newx-center[0])/(newy-center[1]));
+				final double distanceline = Finaldistance.disttocurve(new double[] { newx, newy }, realpos, newy,
+						-(newx - center[0]) / (newy - center[1]));
 				if (distanceline <= mindistance) {
 					mindistance = distanceline;
 					actualposition[0] = newx;
@@ -85,17 +90,13 @@ public class PullwithFastNormal {
 					distance = Finaldistance.Generalfunctiondist(actualposition, realpos);
 					secmindistance = Math.min(distance, secmindistance);
 				}
-				
-				
-				intensity = (1 / (sigma * Math.sqrt(2 * Math.PI))) * Math.exp(-secmindistance * secmindistance / (2 * sigmasq));
+
+				intensity = (1 / (sigma * Math.sqrt(2 * Math.PI)))
+						* Math.exp(-secmindistance * secmindistance / (2 * sigmasq));
 				outbound.get().setReal(intensity);
-				
-				
+
 			}
-			
-	
 		}
-		
 
 	}
 
@@ -105,7 +106,7 @@ public class PullwithFastNormal {
 		double[] max = { 100, 100 };
 
 		final double ratio = (max[1] - min[1]) / (max[0] - min[0]);
-		final long sizeX = 200;
+		final long sizeX = 400;
 		final long sizeY = Math.round(sizeX * ratio);
 
 		final Img<FloatType> houghimage = new ArrayImgFactory<FloatType>().create(new long[] { sizeX, sizeY },
@@ -116,7 +117,7 @@ public class PullwithFastNormal {
 		long totalTime = endTime - startTime;
 		System.out.println("Normal line finding time :" + totalTime);
 
-		// new ImageJ();
+		new ImageJ();
 		ImageJFunctions.show(houghimage).setTitle("Pull Normal line finding function");
 
 	}
