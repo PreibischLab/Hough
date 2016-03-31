600ee90359b438ffc81846e7f053f11d1b410cb0
kapoorlab <kapoorconsciousness@gmail.com>
2016-03-31 19:26:45 +0200

    Code for drawing a circle by pull (best till now)

--- src/main/java/varun/PullwithFastNormal.java
+++ src/main/java/varun/PullwithFastNormal.java
@@ -27,28 +27,18 @@ public class PullwithFastNormal {
 	public static <T extends RealType<T>> void pull(RandomAccessibleInterval<T> imgout, double[] min, double[] max) {
 
 		int n = imgout.numDimensions();
-
 		final double[] position = new double[n];
-
 		final double[] realpos = new double[n];
-
 		double[] actualposition = new double[n];
-		
-
 		double sigmasq = 0, sigma;
-
 		double[] delta = new double[n];
 
 		for (int d = 0; d < n; ++d) {
-
-			delta[d] = (max[d] - min[d]) / (imgout.realMax(d)-imgout.realMin(d)+1);
-
+			delta[d] = (max[d] - min[d]) / (imgout.realMax(d) - imgout.realMin(d) + 1);
 			sigmasq += Math.pow(delta[d], 2);
-
 		}
 
 		sigma = Math.sqrt(sigmasq);
-
 		System.out.println(sigma);
 
 		double[] center = { 0, 0 };
@@ -57,12 +47,11 @@ public class PullwithFastNormal {
 		final RandomAccess<T> outbound = imgout.randomAccess();
 		final RandomAccess<T> circle = imgout.randomAccess();
 		double[] gradient = new double[n];
-		
-
+		// Select a point and assign its pixel value to the distance from the
+		// curve
 		while (inputcursor.hasNext()) {
 			inputcursor.fwd();
 			inputcursor.localize(position);
-
 			for (int d = 0; d < n; ++d)
 				realpos[d] = position[d] * delta[d] + min[d];
 
@@ -70,56 +59,64 @@ public class PullwithFastNormal {
 
 			double mindistance = Double.MAX_VALUE;
 			double secmindistance = Double.MAX_VALUE;
-			
+
 			double distance = 0;
 			double intensity = 0;
-			double step, newstep=0;
 			double initheta = 0;
 			double theta = initheta;
-			double dtheta=1;
-
-
-			position[0] = radius*Math.cos(Math.toRadians(initheta));
-			position[1] = radius*Math.sin(Math.toRadians(initheta));
-			
-			for (int d = 0; d < n; ++d)
-				circle.setPosition(Math.round(center[d] + position[d]), d);
-			
-			
-			 double[] newpos = new double [n];
-			 
-			for(int d=0; d<n;++d)
-             newpos[d] = circle.getDoublePosition(d);
-			
+			double dtheta = 1;
+			double[] newpos = new double[n];
+			// Compute the Normal to the curve on which the input point lies
 			while (true) {
-			    theta = initheta+dtheta;
 
-			    gradient[0] = -radius*Math.sin(Math.toRadians(theta))*Math.toRadians(dtheta);
-				gradient[1] = radius*Math.cos(Math.toRadians(theta))*Math.toRadians(dtheta);
-				
-				for (int d = 0; d < n; ++d){
-					circle.move(Math.round(gradient[d]),d);
+				// Initialize a point on the circle
+				position[0] = radius * Math.cos(Math.toRadians(initheta));
+				position[1] = radius * Math.sin(Math.toRadians(initheta));
+
+				for (int d = 0; d < n; ++d) {
+					circle.setPosition(Math.round(center[d] + position[d]), d);
 					newpos[d] = circle.getDoublePosition(d);
 				}
-				 double distanceline = Finaldistance.disttocurve(new double[] { newpos[0], newpos[1] }, realpos, newpos[1],
-						-(newpos[0] - center[0]) / (newpos[1] - center[1]));
+				// Compute the gradient for incrementing current position
+				
+				theta = initheta + dtheta;
+				gradient[0] = -radius * Math.sin(Math.toRadians(theta)) * Math.toRadians(dtheta);
+				gradient[1] = radius * Math.cos(Math.toRadians(theta)) * Math.toRadians(dtheta);
+
+				// Move the current point along the gradient
+				for (int d = 0; d < n; ++d)
+					circle.move(Math.round(gradient[d]), d);
+				// Get the updated position of the point along the circle
+				for (int d = 0; d < n; ++d) {
+					circle.setPosition(Math.round(center[d] + position[d] + gradient[d]), d);
+					newpos[d] = circle.getDoublePosition(d);
+				}
+
+				// Compute the Normal line at the point
+				double distanceline = Finaldistance.disttocurve(new double[] { newpos[0], newpos[1] }, realpos,
+						newpos[1], -(newpos[0] - center[0]) / (newpos[1] - center[1]));
+				// If the point lies on the Normal compute distance bw it and
+				// input point
 				if (distanceline <= mindistance) {
 					mindistance = distanceline;
 					for (int d = 0; d < n; ++d)
-					actualposition[d] = newpos[d];
+						actualposition[d] = newpos[d];
 					distance = Finaldistance.Generalfunctiondist(actualposition, realpos);
+					// Store current distance and for multiple normal case get
+					// the closest distance
+					if (distance < secmindistance)
+						secmindistance = distance;
 				}
-				if (distance < secmindistance)
-					secmindistance = distance;
+
 				intensity = (1 / (sigma * Math.sqrt(2 * Math.PI)))
 						* Math.exp(-secmindistance * secmindistance / (2 * sigmasq));
 				outbound.get().setReal(intensity);
-				
-				initheta= theta;
-				
-				if (theta>=180)
+
+				initheta = theta;
+
+				if (theta >= 360)
 					break;
-				
+
 			}
 		}
 
