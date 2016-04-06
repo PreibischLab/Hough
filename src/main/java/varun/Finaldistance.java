67c650b435afd8855e0d622892fd6dd65d1040ba
kapoorlab <kapoorconsciousness@gmail.com>
2016-04-06 12:33:17 +0200

    Updating code files

--- src/main/java/varun/Finaldistance.java
+++ src/main/java/varun/Finaldistance.java
@@ -10,10 +10,7 @@ public abstract class Finaldistance {
 	double[] funcparamone;
 	double funcparamtwo;
 	double funcparamthree;
-	double funcparamfour;
-	double funcparamfive;
-	double funcparamsix;
-
+	
 	public Finaldistance(double[] realpoints, double[] funcparamone, double funcparamtwo, double funcparamthree) {
 
 		this.realpoints = realpoints;
@@ -31,61 +28,38 @@ public abstract class Finaldistance {
 
 	}
 
-	public Finaldistance(double[] realpoints, double funcparamtwo, double funcparamthree, double funcparamfour) {
-
-		this.realpoints = realpoints;
-		this.funcparamtwo = funcparamtwo;
-		this.funcparamthree = funcparamthree;
-		this.funcparamfour = funcparamfour;
-
-	}
-	
-	public Finaldistance(double[] realpoints, double funcparamtwo, double funcparamthree, double funcparamfour, double funcparamfive) {
-
-		this.realpoints = realpoints;
-		this.funcparamtwo = funcparamtwo;
-		this.funcparamthree = funcparamthree;
-		this.funcparamfour = funcparamfour;
-		this.funcparamfive = funcparamfive;
-
-	}
-	
-	public Finaldistance(double[] realpoints, double funcparamtwo, double funcparamthree, double funcparamfour, 
-			double funcparamfive, double funcparamsix) {
-
-		this.realpoints = realpoints;
-		this.funcparamtwo = funcparamtwo;
-		this.funcparamthree = funcparamthree;
-		this.funcparamfour = funcparamfour;
-		this.funcparamfive = funcparamfive;
-		this.funcparamsix = funcparamsix;
-
-	}
+	// General distance between two points
 	public static double Generalfunctiondist(double[] secondpos, double[] firstpos) {
-		double distance ;
-	
+		double distance;
+
 		distance = Math.pow((secondpos[0] - firstpos[0]), 2) + Math.pow((secondpos[1] - firstpos[1]), 2);
 
 		return Math.sqrt(distance);
-	}	
+	}
 
-	public static <T extends RealType<T>> double  disttocurve(double[] secondrealpos,double[] realpos,
-			double functionvalue, double functionderiv){
-            //x= secondrealpos[0], f(x) = function value or = y for implicit curves
-		Finalfunction Normalline = new Finalfunction(realpos, -1.0 / functionderiv,
-				secondrealpos[0] / functionderiv + functionvalue);
-	final double	 distanceline = Normalline.Linefunctiondist();
+	// Ditance of a point (realpos) from the Normal line to a curve at some point (secondrealpos), derivative of the function
+	// is also needed at the same point
 	
-	return distanceline;
+	public static <T extends RealType<T>> double disttocurvenormal(double[] secondrealpos, double[] realpos,
+			 double [] derivrealpos ) {
+
+		Finalfunction Normalline = new Finalfunction(realpos, -1.0 / derivrealpos[1],
+				secondrealpos[0] / derivrealpos[1] + secondrealpos[1]);
+		final double distanceline = Normalline.Linefunctiondist();
+
+		return distanceline;
 	}
 	
-	public static <T extends RealType<T>> double  disttocurvetangent(double[] secondrealpos,double[] realpos,
-			double functionvalue, double functionderiv){
-		Finalfunction Tangentline = new Finalfunction(realpos,  functionderiv,
-				 functionvalue- secondrealpos[0]*functionderiv);
-	final double	 distanceline = Tangentline.Linefunctiondist();
+	// Ditance of a point (realpos) from the Tangent line to a curve at some point (secondrealpos), derivative of the function
+	// is also needed at the same point
 	
-	return distanceline;
+	public static <T extends RealType<T>> double disttocurvetangent(double[] secondrealpos, double[] realpos,
+			double[] derivrealpos) {
+		Finalfunction Tangentline = new Finalfunction(realpos, derivrealpos[1],
+				secondrealpos[1] - secondrealpos[0] * derivrealpos[1]);
+		final double distanceline = Tangentline.Linefunctiondist();
+
+		return distanceline;
 	}
-	
+
 }
--- src/main/java/varun/Finalfunction.java
+++ src/main/java/varun/Finalfunction.java
@@ -14,27 +14,7 @@ public class Finalfunction extends Finaldistance {
 
 	}
 
-	public Finalfunction(double[] realpoints, double funcparamtwo, double funcparamthree, double funcparamfour) {
-
-		super(realpoints, funcparamtwo, funcparamthree, funcparamfour);
-
-	}
-	
-	public Finalfunction(double[] realpoints, double funcparamtwo, double funcparamthree, double funcparamfour,
-			double funcparamfive) {
-
-		super(realpoints, funcparamtwo, funcparamthree, funcparamfour, funcparamfive);
-
-	}
-	
-	public Finalfunction(double[] realpoints, double funcparamtwo, double funcparamthree, double funcparamfour,
-			double funcparamfive, double funcparamsix) {
-
-		super(realpoints, funcparamtwo, funcparamthree, funcparamfour, funcparamfive, funcparamsix);
-
-	}
-	
-
+// Shortest distance of a point from a user defined circle
 	public double Circlefunctiondist() {
 
 		// funcparamone = center fo the circle, funcparamtwo = radius;
@@ -48,10 +28,10 @@ public class Finalfunction extends Finaldistance {
 		return distance;
 
 	}
-
+	// Shortest distance of a point from a user defined line
 	public double Linefunctiondist() {
 
-		// funcparamtwo = slope, funcparamthree = constant
+		// funcparamtwo = slope, funcparamthree = intercept along x-axis
 
 		double distance;
 
@@ -64,175 +44,5 @@ public class Finalfunction extends Finaldistance {
 		return Math.sqrt(distance);
 	}
 
-	public double Sinfunction() {
-		double function;
-
-		function = funcparamtwo * Math.sin(Math.toRadians(realpoints[0] + funcparamthree));
-
-		return function;
-	}
 	
-	public double DerivSinfunction() {
-		double function;
-
-		function = funcparamtwo  * Math.cos(Math.toRadians(realpoints[0] + funcparamthree));
-
-		return function;
-	}
-	
-    public double Gaussianfunction(){
-    	double function;
-    	
-    	function = funcparamtwo*Math.exp(-realpoints[0]*realpoints[0]/funcparamthree);
-    	
-    	return function;
-    }
-    
-    public double DerivGaussianfunction(){
-    	double function;
-    	
-    	function = funcparamtwo*(-2*realpoints[0]/(Math.pow(funcparamthree, 2)))*Math.exp(-realpoints[0]*realpoints[0]/funcparamthree);
-    	
-    	return function;
-    }
-	
-	public double HalfCirclefunction(){
-		double function;
-		
-		function = Math.sqrt((funcparamtwo*funcparamtwo-Math.pow(realpoints[0]-funcparamone[0],2)))+funcparamone[1];
-		
-		return function;
-	}
-	
-	public double DerivHalfCirclefunction(){
-		double function;
-		
-		function = -(realpoints[0]-funcparamone[0])/Math.sqrt((funcparamtwo*funcparamtwo-
-				Math.pow(realpoints[0]-funcparamone[0],2)));
-		return function;
-	}
-	
-	public double OtherHalfCirclefunction(){
-		double function;
-		
-		function = -Math.sqrt(funcparamtwo*funcparamtwo-Math.pow(realpoints[0]-funcparamone[0],2))+funcparamone[1];
-		
-		return function;
-	}
-	
-	public double DerivOtherHalfCirclefunction(){
-		double function;
-		
-		function = (realpoints[0]-funcparamone[0])/Math.sqrt(funcparamtwo*funcparamtwo-Math.pow(realpoints[0]-funcparamone[0],2));
-		return function;
-	}
-	
-
-	
-	
-	public double Quadfunction() {
-		double function;
-		
-		function = funcparamtwo*Math.pow(realpoints[0], 2)+funcparamthree*realpoints[0] + funcparamfour;
-		
-		return function;
-	}
-
-	public double DerivQuadfunction(){
-		double function;
-		
-		function = 2*funcparamtwo*realpoints[0] + funcparamthree;
-		
-		return function;
-	}
-	public double DerivsecQuadfunction(){
-		double function;
-		
-		function = 2*funcparamtwo;
-		
-		return function;
-	}
-	
-	
-	public double Cubicfunction(){
-		double function;
-		
-		function = funcparamtwo*Math.pow(realpoints[0], 3) + funcparamthree*Math.pow(realpoints[0], 2) 
-		         + funcparamfour*realpoints[0] + funcparamfive;
-		
-		return function;
-	}
-	
-	public double DerivCubicfunction(){
-		double function;
-		
-		function = 3*funcparamtwo*Math.pow(realpoints[0], 2) + 2*funcparamthree*realpoints[0] + funcparamfour;
-		
-		return function;
-	}
-	
-	public double DerivsecCubicfunction(){
-		double function;
-		
-		function = 6*funcparamtwo*realpoints[0] + 2*funcparamthree;
-		
-		return function;
-	}
-	
-	public double Biquadfunction(){
-		double function;
-		
-		function = funcparamtwo*Math.pow(realpoints[0], 4) + funcparamthree*Math.pow(realpoints[0], 3) 
-		         + funcparamfour*Math.pow(realpoints[0], 2) + funcparamfive*realpoints[0] +funcparamsix ;
-		
-		return function;
-	}
-	
-	public double DerivBiquadfunction(){
-		double function;
-		
-		function = 4*funcparamtwo*Math.pow(realpoints[0], 3) + 3*funcparamthree*Math.pow(realpoints[0], 2) 
-		         + 2*funcparamfour*realpoints[0] + funcparamfive;
-		
-		return function;
-	}
-	
-	
-	
-	public double Mysin(double x){
-		double function;
-		
-		function = x - Math.pow(x, 3)/6 + Math.pow(x, 5)/120 -Math.pow(x, 7)/5040;  
-		
-		return function;
-	}
-	
-	public double Mycos(double x){
-		double function;
-		
-		function = 1 - Math.pow(x, 2)/2 + Math.pow(x, 4)/24 - Math.pow(x, 6)/720;
-		
-		return function;
-	}
-	/** Quadratic function, x=secondrealpos, A*x^2+B*x+C **/
-/*	
-	Finalfunction Quadfunction = new Finalfunction(secondrealpos, 0.01,0.02, 4);
-	final double functionvalue = Quadfunction.Quadfunction();
-	final double functionderiv = Quadfunction.DerivQuadfunction();
-	
-*/
-	/** Cubic function, x=secondrealpos, A*x^3+B*x^2+C*x+D **/
-/*	
-	Finalfunction Cubicfunction = new Finalfunction(secondrealpos, 1.1,0.1,-4.1, 4);
-	final double functionvalue = Cubicfunction.Cubicfunction();
-	final double functionderiv = Cubicfunction.DerivCubicfunction();
-	
-	/** Biquad function, x=secondrealpos, A*x^4+B*x^3+C*x^2+D*x+E **/
-/*	
-	Finalfunction Biquadfunction = new Finalfunction(secondrealpos,2.1,-2.4,-2.4,1.8,4.0);
-	final double functionvalue = Biquadfunction.Biquadfunction();
-	final double functionderiv = Biquadfunction.DerivBiquadfunction();
-*/	
-	
-
 }
