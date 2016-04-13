20bfae0ab160505e1d53d7fead01dd0a99d65263
kapoorlab <kapoorconsciousness@gmail.com>
2016-04-13 12:34:04 +0200

    Overlay reconstructed line on original line, 6 degree angle error

--- src/main/java/varun/HoughPushCurves.java
+++ src/main/java/varun/HoughPushCurves.java
@@ -1,6 +1,7 @@
 package varun;
 
 import java.awt.Color;
+import java.awt.Image;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
@@ -8,6 +9,7 @@ import java.util.concurrent.Executors;
 
 import net.imglib2.algorithm.dog.DifferenceOfGaussian;
 import net.imglib2.algorithm.dog.DogDetection;
+import net.imglib2.algorithm.localextrema.RefinedPeak;
 import ij.ImageJ;
 import ij.ImagePlus;
 import ij.ImageStack;
@@ -68,8 +70,8 @@ public class HoughPushCurves {
 		new ImageJ();
 		ImageJFunctions.show(inputimg);
 		// Set size of pixels in Hough space
-		double thetaPerPixel = 0.5;
-		double rhoPerPixel = 0.5;
+		double thetaPerPixel = 0.2;
+		double rhoPerPixel = 0.2;
 
 		int mintheta = 0;
 		int maxtheta = 180;
@@ -88,15 +90,15 @@ public class HoughPushCurves {
 		// Size of Hough space
 		FinalInterval interval = new FinalInterval(new long[] { pixelsTheta, (long) (pixelsRho * ratio) });
 		final Img<FloatType> houghimage = new ArrayImgFactory<FloatType>().create(interval, new FloatType());
-		
-		ArrayList<Point> Minlist = new ArrayList<Point>(inputimg.numDimensions());
+
+		ArrayList<RefinedPeak<Point>> SubpixelMinlist =  new ArrayList<RefinedPeak<Point>>(inputimg.numDimensions());
 		FloatType val = new FloatType(100);
-		
+
 		final double[] sizes = new double[inputimg.numDimensions()];
-		
+
 		for (int d = 0; d < houghimage.numDimensions(); ++d)
 			sizes[d] = houghimage.dimension(d);
-		
+
 		// Do the Hough transform
 		Houghspace(inputimg, houghimage, min, max, val);
 
@@ -104,95 +106,49 @@ public class HoughPushCurves {
 
 		// Create a Dog Detection object in Hough space
 		DogDetection<FloatType> newdog = new DogDetection<FloatType>(Views.extendMirrorSingle(houghimage), interval,
-				new double[] {pixelsTheta, pixelsRho},
-				1.1, 1.1*1.1, DogDetection.ExtremaType.MINIMA, 10, false);
-		
+				new double[] { pixelsTheta, pixelsRho }, 1.1, 1.1 * 1.1, DogDetection.ExtremaType.MINIMA, 10, false);
+
 		// Detect minima in Scale space
-		Minlist = newdog.getPeaks();
-		
+		SubpixelMinlist = newdog.getSubpixelPeaks();
+
 		double[] points = new double[inputimg.numDimensions()];
-		for (int index = 0; index < Minlist.size(); ++index) {
-
-			points = TransformCordinates.transformfwd(new double[] { Minlist.get(index).getDoublePosition(0),
-					Minlist.get(index).getDoublePosition(1) }, sizes, min, max);
-
-			System.out.println(" Found Peaks at :" +points[0] + " " + points[1]);
-			
-		}
 		
+		ImageStack stack = new ImageStack((int)inputimg.dimension(0), (int)inputimg.dimension(1));
 		
-		
-	/*
-	 * // Gettinglocal maximas
-		final int numthreads = Runtime.getRuntime().availableProcessors();
-		final ExecutorService service = Executors.newFixedThreadPool(numthreads);
-
-		ImageStack stack = new ImageStack((int) houghimage.dimension(0), (int) houghimage.dimension(1));
-		
-	 
-		for (double s = 1.1; s <= 15; s = s + 0.5) {
-			System.out.println(s);
-			final double[] sigmaA = { s, s };
-			final double[] sigmaB = { sigmaA[0] * sigmaA[0], sigmaA[1] * sigmaA[1] };
-			final Img<FloatType> dogimage = new ArrayImgFactory<FloatType>().create(interval, new FloatType());
-			DifferenceOfGaussian.DoG(sigmaA, sigmaB, Views.extendMirrorSingle(houghimage), tmpdogimage, dogimage,
-					service);
-			stack.addSlice(ImageJFunctions.wrap(dogimage, "sigma=" + s).getProcessor());
-			Minlist = GetLocalmaxmin.FindLocalMinima(dogimage);
-
-			double[] points = new double[inputimg.numDimensions()];
-			for (int index = 0; index < Minlist.size(); ++index) {
-
-				points = TransformCordinates.transformfwd(new double[] { Minlist.get(index).getDoublePosition(0),
-						Minlist.get(index).getDoublePosition(1) }, sizes, min, max);
-
-				System.out.println(points[0] + " " + points[1]);
-				System.out.println(
-						Minlist.get(index).getDoublePosition(0) + " " + Minlist.get(index).getDoublePosition(1));
-			}
-		}
-
+		stack.addSlice(ImageJFunctions.wrap(inputimg, "").getProcessor());
 		new ImageJ();
 
-		ImagePlus imp = new ImagePlus("scale space hough", stack);
+		ImagePlus imp= new ImagePlus( "scale space hough", stack );
 		imp.show();
-		/*
-		 * // localminimage = GetLocalmaxmin.FindandDisplayLocalMinima(dogimage,
-		 * new ArrayImgFactory<FloatType>());
-		 * 
-		 * new ImageJ(); ImageJFunctions.show(localminimage);
-		 * 
-		 * // Minlist = GetLocalmaxmin.FindLocalMinima(dogimage); final double[]
-		 * sizes = new double[inputimg.numDimensions()]; RealCursor<FloatType>
-		 * listcursor = Minlist.localizingCursor(); for (int d = 0; d <
-		 * houghimage.numDimensions(); ++d) sizes[d] = houghimage.dimension(d);
-		 * 
-		 * double[] points = new double[inputimg.numDimensions()]; while
-		 * (listcursor.hasNext()) { listcursor.fwd(); points =
-		 * TransformCordinates.transformfwd( new double[] {
-		 * listcursor.getDoublePosition(0), listcursor.getDoublePosition(1) },
-		 * sizes, min, max);
-		 * 
-		 * System.out.println(points[0]+ " "+ points[1]);
-		 * System.out.println(listcursor.getDoublePosition(0)+ " "+
-		 * listcursor.getDoublePosition(1)); }
-		 * 
-		 * /* ImagePlus imp= new ImagePlus( "scale space hough", stack );
-		 * imp.show();
-		 * 
-		 * Overlay o = imp.getOverlay();
-		 * 
-		 * if ( o == null ) { o = new Overlay(); imp.setOverlay( o ); }
-		 * 
-		 * o.clear();
-		 * 
-		 * for ( int i = 0; i < 2; ++i ) { Line l = new Line(10, 10, 100, 100 +
-		 * i * 2); l.setStrokeColor( new Color( i, 128, 128 ) );
-		 * l.setStrokeWidth(0.5); o.add( l ); }
-		 * 
-		 * 
-		 * imp.updateAndDraw();
-		 */
+
+		Overlay o = imp.getOverlay();
+		
+		if ( o == null )
+		{
+			o = new Overlay();
+			imp.setOverlay( o );
+		}
+		
+		o.clear();
+		
+		
+		for (int index = 0; index < SubpixelMinlist.size(); ++index) {
+
+			points = TransformCordinates.transformfwd(
+					new double[] { SubpixelMinlist.get(index).getDoublePosition(0), SubpixelMinlist.get(index).getDoublePosition(1) },
+					sizes, min, max);
+
+			System.out.println(" Found Peaks at :" + "Theta: " + points[0] + " Rho: " + points[1]);
+			
+			Line newline = new Line(0, points[1]/Math.sin(Math.toRadians(points[0])), inputimg.dimension(0)
+					, points[1]/Math.sin(Math.toRadians(points[0]))-inputimg.dimension(0)/Math.tan(Math.toRadians(points[0])));
+			newline.setStrokeColor( new Color( index, 128, 128 ) );
+			newline.setStrokeWidth(0.5);
+			o.add( newline );
+		}
+		imp.updateAndDraw();
+
+		
 	}
 
 }
--- src/main/java/varun/PushCurves.java
+++ src/main/java/varun/PushCurves.java
@@ -22,135 +22,220 @@ public class PushCurves {
 		double[] newpos = new double[n];
 		double[] backpos = new double[n];
 		double[] sigma = new double[n];
-        final RandomAccess<FloatType> outbound = imgout.randomAccess();
+		final RandomAccess<FloatType> outbound = imgout.randomAccess();
 		double stepsize = 0.1;
 		int[] setpos = new int[n];
 		for (int d = 0; d < n; ++d)
 			size[d] = imgout.dimension(d);
-		
+
 		Cursor<FloatType> cursor = Views.iterable(imgout).localizingCursor();
 		while (cursor.hasNext()) {
 			cursor.fwd();
 			cursor.localize(location);
 			realpos = TransformCordinates.transformfwd(location, size, min, max);
-			
+
 			// To get a starting point on the circle
 			if (Math.pow(realpos[0] - center[0], 2) + Math.pow(realpos[1] - center[1], 2)
-					- radius * radius <=1.0E-50) {
+					- radius * radius <= 1.0E-50) {
 				for (int d = 0; d < n; ++d)
 					position[d] = realpos[d];
 				break;
-				
+
 			}
-			
+
 		}
-		
+
 		for (int d = 0; d < n; ++d)
 			iniposition[d] = position[d];
 
-		double initheta =  Math.atan2(iniposition[1]-center[1],iniposition[0]-center[0]);
-		double increment = Math.acos((2*radius*radius-stepsize*stepsize)/(2*radius*radius));
+		double initheta = Math.atan2(iniposition[1] - center[1], iniposition[0] - center[0]);
+		double increment = Math.acos((2 * radius * radius - stepsize * stepsize) / (2 * radius * radius));
 
 		backini = TransformCordinates.transformback(iniposition, size, min, max);
 		sigma[0] = 1;
 		sigma[1] = 1;
 		while (true) {
-			
+
 			// Move the current point along the curve
-				
-				newpos[0] = center[0]+radius* Math.cos((initheta-increment));
-				newpos[1] = center[1]+radius* Math.sin((initheta-increment));
-				initheta = Math.atan2(newpos[1]-center[1],newpos[0]-center[0]);
-			
+
+			newpos[0] = center[0] + radius * Math.cos((initheta - increment));
+			newpos[1] = center[1] + radius * Math.sin((initheta - increment));
+			initheta = Math.atan2(newpos[1] - center[1], newpos[0] - center[0]);
+
 			// Transform the co-ordinates back as double[]
 			backpos = TransformCordinates.transformback(newpos, size, min, max);
-			
+
 			setpos[0] = (int) Math.round(backpos[0]);
 			setpos[1] = (int) Math.round(backpos[1]);
 
 			// To set the pixel intensity
 			AddGaussian.addGaussian(imgout, backpos, sigma, false);
 
-			// To make sure that the values transformed back are not out of bounds
-			if (backpos[0] < imgout.realMax(0) - imgout.realMin(0) || backpos[0] > imgout.realMin(0) || backpos[1] < imgout.realMax(1) - imgout.realMin(1)
-					|| backpos[1] > imgout.realMin(1))
+			// To make sure that the values transformed back are not out of
+			// bounds
+			if (backpos[0] < imgout.realMax(0) - imgout.realMin(0) || backpos[0] > imgout.realMin(0)
+					|| backpos[1] < imgout.realMax(1) - imgout.realMin(1) || backpos[1] > imgout.realMin(1))
 
 				outbound.setPosition(setpos);
 
 			// Stopping criteria of moving along the circular arc
-			if ( Math.abs(setpos[0]-(int) Math.round(backini[0]))==0 &&  Math.abs(setpos[1]-(int) Math.round(backini[1]))==0   )
+			if (Math.abs(setpos[0] - (int) Math.round(backini[0])) == 0
+					&& Math.abs(setpos[1] - (int) Math.round(backini[1])) == 0)
 				break;
-			
-			// General Stopping criteria of moving along a curve, when we hit a boundary
+
+			// General Stopping criteria of moving along a curve, when we hit a
+			// boundary
 			if (newpos[0] >= max[0] || newpos[0] <= min[0] || newpos[1] >= max[1] || newpos[1] <= min[1])
 
 				break;
-			
 
 		}
 	}
-	
-	public static void DrawSine(Img<FloatType> imgout, double[] min, double[] max, double amplitude, double phase ){
-		
-            int n = imgout.numDimensions();		
-            double[] size = new double[n];
-    		double[] position = new double[n];
-    		double[] newpos = new double[n];
-    		double[] backpos = new double[n];
-    		double[] sigma = new double[n];
-    		double increment;
-    		final RandomAccess<FloatType> outbound = imgout.randomAccess();
-			double stepsize = 0.1;
-			int[] setpos = new int[n];
+
+	public static void DrawSine(Img<FloatType> imgout, double[] min, double[] max, double amplitude, double phase) {
+
+		int n = imgout.numDimensions();
+		double[] size = new double[n];
+		double[] position = new double[n];
+		double[] newpos = new double[n];
+		double[] backpos = new double[n];
+		double[] sigma = new double[n];
+		double increment;
+		final RandomAccess<FloatType> outbound = imgout.randomAccess();
+		double stepsize = 0.1;
+		int[] setpos = new int[n];
+		for (int d = 0; d < n; ++d)
+			size[d] = imgout.dimension(d);
+
+		// Starting position, for explicit curves its easier to choose a
+		// starting point
+		position[0] = min[0];
+		position[1] = amplitude * Math.sin(Math.toRadians(position[0] + phase));
+
+		sigma[0] = 0.7;
+		sigma[1] = 0.7;
+		while (true) {
+			increment = stepsize * amplitude * Math.cos(Math.toRadians(position[0] + phase));
+			// Increment from starting position (min) towards max
+			newpos[0] = position[0] + stepsize;
+			newpos[1] = amplitude * Math.sin(Math.toRadians(position[0] + phase)) + increment;
+			// amplitude*Math.sin(Math.toRadians(newpos[0]+phase));
+
 			for (int d = 0; d < n; ++d)
-				size[d] = imgout.dimension(d);
-
-			// Starting position, for explicit curves its easier to choose a starting point
-			position[0] = min[0];
-			position[1] = amplitude*Math.sin(Math.toRadians(position[0]+phase));
-			
-			
-			sigma[0] = 0.7;
-			sigma[1] = 0.7;
-			while(true){
-				increment = stepsize*amplitude*Math.cos(Math.toRadians(position[0]+phase));
-				// Increment from starting position (min) towards max
-				newpos[0] = position[0] + stepsize;
-				newpos[1] = amplitude*Math.sin(Math.toRadians(position[0]+phase)) + increment;
-						//amplitude*Math.sin(Math.toRadians(newpos[0]+phase));
-				
-				for (int d =0; d<n;++d)
 				position[d] = newpos[d];
-				
-				
-				// Transform the co-ordinates back as double[]
-				backpos = TransformCordinates.transformback(newpos, size, min, max);
-				
-				setpos[0] = (int) Math.round(backpos[0]);
-				setpos[1] = (int) Math.round(backpos[1]);
 
-				// To set the pixel intensity
-				AddGaussian.addGaussian(imgout, backpos, sigma, false);
+			// Transform the co-ordinates back as double[]
+			backpos = TransformCordinates.transformback(newpos, size, min, max);
 
-				
-				// To make sure that the values transformed back are not out of bounds
-				if (backpos[0] < imgout.realMax(0) - imgout.realMin(0) || backpos[0] > imgout.realMin(0) || backpos[1] < imgout.realMax(1) - imgout.realMin(1)
-						|| backpos[1] > imgout.realMin(1))
+			setpos[0] = (int) Math.round(backpos[0]);
+			setpos[1] = (int) Math.round(backpos[1]);
 
-					outbound.setPosition(setpos);
-					
-			
-				// General Stopping criteria of moving along a curve, when we hit a boundary
-				if (newpos[0] >= max[0] || newpos[0] <= min[0] || newpos[1] >= max[1] || newpos[1] <= min[1])
+			// To set the pixel intensity
+			AddGaussian.addGaussian(imgout, backpos, sigma, false);
 
-					break;
-				
-				
-					
-				
-			}
-			
-		
+			// To make sure that the values transformed back are not out of
+			// bounds
+			if (backpos[0] < imgout.realMax(0) - imgout.realMin(0) || backpos[0] > imgout.realMin(0)
+					|| backpos[1] < imgout.realMax(1) - imgout.realMin(1) || backpos[1] > imgout.realMin(1))
+
+				outbound.setPosition(setpos);
+
+			// General Stopping criteria of moving along a curve, when we hit a
+			// boundary
+			if (newpos[0] >= max[0] || newpos[0] <= min[0] || newpos[1] >= max[1] || newpos[1] <= min[1])
+
+				break;
+
+		}
+
+	}
+
+	public static void Drawexactcircle(RandomAccessibleInterval<FloatType> imgout, double[] center, double radius,
+			double[] min, double[] max) {
+
+		int n = imgout.numDimensions();
+
+		final double[] position = new double[n];
+
+		final double[] realpos = new double[n];
+
+		double sigmasq = 0, sigma;
+
+		double[] delta = new double[n];
+
+		for (int d = 0; d < n; ++d) {
+
+			delta[d] = (max[d] - min[d]) / (imgout.dimension(d));
+
+			sigmasq += Math.pow(delta[d], 2);
+
+		}
+		sigma = Math.sqrt(sigmasq);
+
+		final Cursor<FloatType> inputcursor = Views.iterable(imgout).localizingCursor();
+
+		final RandomAccess<FloatType> outbound = imgout.randomAccess();
+
+		while (inputcursor.hasNext()) {
+			inputcursor.fwd();
+			inputcursor.localize(position);
+
+			// Forward transformation
+			for (int d = 0; d < n; ++d)
+				realpos[d] = position[d] * delta[d] + min[d];
+			outbound.setPosition(inputcursor);
+			// To set the pixel intensity as the shortest distance to the curve
+			double distance = 0;
+			double intensity = 0;
+
+			Finalfunction circlefunction = new Finalfunction(realpos, center, radius, 0);
+
+			distance = circlefunction.Circlefunctiondist();
+
+			intensity = (1 / (sigma * Math.sqrt(2 * Math.PI))) * Math.exp(-distance * distance / (2 * sigmasq));
+			outbound.get().setReal(intensity);
+
+		}
+	}
+
+	public static void Drawexactline(RandomAccessibleInterval<FloatType> imgout, double slope, double intercept,
+			double[] min, double[] max) {
+
+		int n = imgout.numDimensions();
+		final int[] position = new int[n];
+		final double[] realpos = new double[n];
+		double sigmasq = 0, sigma;
+		double[] delta = new double[n];
+
+		for (int d = 0; d < n; ++d) {
+			delta[d] = (max[d] - min[d]) / (imgout.dimension(d));
+			sigmasq += Math.pow(delta[d], 2);
+
+		}
+		sigma = Math.sqrt(sigmasq);
+		final Cursor<FloatType> inputcursor = Views.iterable(imgout).localizingCursor();
+		final RandomAccess<FloatType> outbound = imgout.randomAccess();
+
+		while (inputcursor.hasNext()) {
+
+			inputcursor.fwd();
+			inputcursor.localize(position);
+			// Forward transformation
+			for (int d = 0; d < n; ++d)
+				realpos[d] = position[d] * delta[d] + min[d];
+			outbound.setPosition(inputcursor);
+			// To set the pixel intensity as the shortest distance to the curve
+			double distance = 0;
+			double intensity = 0;
+
+			Finalfunction linefunction = new Finalfunction(realpos, slope, intercept);
+			distance = linefunction.Linefunctiondist();
+
+			outbound.setPosition(inputcursor);
+
+			intensity = (1 / (sigma * Math.sqrt(2 * Math.PI))) * Math.exp(-distance * distance / (2 * sigmasq));
+			outbound.get().setReal(intensity);
+		}
 	}
 
 }
